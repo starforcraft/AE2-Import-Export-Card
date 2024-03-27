ca085f2e9e5b8970e6c5224a2f3729023f74e284
Starforcraft <starforcraft@yahoo.com>
2024-03-27 15:04:21 +0100

    Fixed compat with AE Infinity Booster mod

--- src/main/java/com/ultramega/ae2insertexportcard/mixin/MixinWirelessTerminalItem.java
+++ src/main/java/com/ultramega/ae2insertexportcard/mixin/MixinWirelessTerminalItem.java
@@ -1,18 +1,15 @@
 package com.ultramega.ae2insertexportcard.mixin;
 
 import appeng.api.config.Actionable;
-import appeng.api.implementations.blockentities.IWirelessAccessPoint;
 import appeng.api.networking.IGrid;
-import appeng.api.networking.IGridNode;
 import appeng.api.stacks.AEItemKey;
 import appeng.api.storage.StorageHelper;
-import appeng.blockentity.networking.WirelessAccessPointBlockEntity;
+import appeng.helpers.WirelessTerminalMenuHost;
 import appeng.items.tools.powered.WirelessTerminalItem;
 import appeng.me.helpers.ChannelPowerSrc;
 import appeng.me.helpers.PlayerSource;
 import com.ultramega.ae2insertexportcard.AE2InsertExportCard;
 import com.ultramega.ae2insertexportcard.item.UpgradeHost;
-import de.mari_023.ae2wtlib.wut.ItemWUT;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -20,7 +17,6 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -29,21 +25,14 @@ import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Mixin({WirelessTerminalItem.class, ItemWUT.class})
+@Mixin(WirelessTerminalItem.class)
 public class MixinWirelessTerminalItem extends Item {
-    @Nullable
-    @Unique
-    private IWirelessAccessPoint ae2InsertExportCard$myWap;
-
     public MixinWirelessTerminalItem(Properties properties) {
         super(properties);
     }
@@ -63,13 +52,18 @@ public class MixinWirelessTerminalItem extends Item {
         //Check if in access point range
         boolean inRange = false;
         IGrid grid = null;
+        WirelessTerminalMenuHost host = null;
 
         if(stack.getItem() instanceof WirelessTerminalItem wirelessTerminalItem) {
             grid = wirelessTerminalItem.getLinkedGrid(stack, level, player);
-            inRange = ae2InsertExportCard$rangeCheck(grid, player, level);
+
+            if(wirelessTerminalItem.getMenuHost(player, slotId, stack, null) instanceof WirelessTerminalMenuHost menuHost) {
+                host = menuHost;
+                inRange = host.rangeCheck();
+            }
         }
 
-        if(!inRange || grid == null) return;
+        if(!inRange) return;
 
         if(stack.getTag().contains("upgrades")) {
             ListTag tagList = stack.getTag().getList("upgrades", Tag.TAG_COMPOUND);
@@ -81,7 +75,7 @@ public class MixinWirelessTerminalItem extends Item {
 
                 if(tag != null && (isInsertUpgrade || isExportUpgrade)) {
                     int[] selectedInventorySlots = tag.getIntArray(UpgradeHost.NBT_SELECTED_INVENTORY_SLOTS);
-                    boolean filterMode = isInsertUpgrade ? tag.getBoolean(UpgradeHost.NBT_FILTER_MODE) : false;
+                    boolean filterMode = isInsertUpgrade && tag.getBoolean(UpgradeHost.NBT_FILTER_MODE);
 
                     for(int j = 0; j < selectedInventorySlots.length; j++) {
                         if(selectedInventorySlots[j] >= 1) {
@@ -98,7 +92,7 @@ public class MixinWirelessTerminalItem extends Item {
                                     }
                                 }
 
-                                var node = this.ae2InsertExportCard$getActionableNode();
+                                var node = host.getActionableNode();
                                 if(node == null) return;
 
                                 if (filters.isEmpty() || (isExportUpgrade || (filterMode == filters.contains(itemInInventory.getItem())))) {
@@ -139,60 +133,5 @@ public class MixinWirelessTerminalItem extends Item {
                 }
             }
         }
-    }
-
-    @Unique
-    public IGridNode ae2InsertExportCard$getActionableNode() {
-        if (this.ae2InsertExportCard$myWap != null) {
-            return this.ae2InsertExportCard$myWap.getActionableNode();
-        }
-        return null;
-    }
-
-    @Unique
-    protected boolean ae2InsertExportCard$rangeCheck(IGrid targetGrid, Player player, Level level) {
-        if (targetGrid != null) {
-            @Nullable
-            IWirelessAccessPoint bestWap = null;
-            double bestSqDistance = Double.MAX_VALUE;
-
-            // Find closest WAP
-            for (var wap : targetGrid.getMachines(WirelessAccessPointBlockEntity.class)) {
-                double sqDistance = ae2InsertExportCard$getWapSqDistance(wap, player, level);
-
-                // If the WAP is not suitable then MAX_VALUE will be returned and the check will fail
-                if (sqDistance < bestSqDistance) {
-                    bestSqDistance = sqDistance;
-                    bestWap = wap;
-                }
-            }
-
-            // If no WAP is found this will work too
-            this.ae2InsertExportCard$myWap = bestWap;
-            return this.ae2InsertExportCard$myWap != null;
-        }
-
-        return false;
-    }
-
-    @Unique
-    protected double ae2InsertExportCard$getWapSqDistance(IWirelessAccessPoint wap, Player player, Level level) {
-        double rangeLimit = wap.getRange();
-        rangeLimit *= rangeLimit;
-
-        var dc = wap.getLocation();
-
-        if (dc.getLevel() == level) {
-            var offX = dc.getPos().getX() - player.getX();
-            var offY = dc.getPos().getY() - player.getY();
-            var offZ = dc.getPos().getZ() - player.getZ();
-
-            double r = offX * offX + offY * offY + offZ * offZ;
-            if (r < rangeLimit && wap.isActive()) {
-                return r;
-            }
-        }
-
-        return Double.MAX_VALUE;
     }
 }
